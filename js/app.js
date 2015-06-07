f6b22d5fa914bd152c3637ff5b47cbbaaf76e744
Sylvain UTARD <sylvain.utard@gmail.com>
2015-06-07 17:16:47 +0200

    Update 2015-06-07T15:16:45.287Z

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ app.controller('SearchCtrl', ['$scope', '$sce', '$timeout', 'algolia', SearchCtr
 app.filter('facetTitle', facet.titleFilter);
 app.filter('facetValue', facet.valueFilter);
 
-}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_d3c6cd5f.js","/")
+}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_84e6f5e7.js","/")
 },{"./controllers/SearchCtrl":1,"./filters/facet":3,"algoliasearch/src/browser/builds/algoliasearch.angular":92,"angular":101,"angular-sanitize":99,"buffer":102,"oMfpAn":107}],3:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 "use strict";
