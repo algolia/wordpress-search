592441e4d33f8c6bfe5c1d6e90084ff1a9e17c8d
Sylvain UTARD <sylvain.utard@gmail.com>
2015-08-12 17:44:25 -0700

    Cosmetics improvements

--- app/scripts/controllers/SearchCtrl.js
+++ app/scripts/controllers/SearchCtrl.js
@@ -100,7 +100,7 @@ var SearchCtrl = function($scope, $sce, $timeout, $location, algolia) {
     blurring && $timeout.cancel(blurring);
     blurring = $timeout(function() {
       unblur(delayedContent);
-    }, 200);
+    }, 50);
 
     $scope.helper.setQuery(q).search();
   });
